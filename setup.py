414211ddc2c3bc8dfd5b4ff8feb14a3d88bed435
Tobias Schmid <toashd@gmail.com>
2014-12-29 19:41:16 +0100

    Add short helper tasks to publish and tag

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
 from setuptools import setup, find_packages
 
+version = "1.0.0"
+
+# python setup.py tag
+if sys.argv[-1] == 'tag':
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+# python setup.py publish
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    sys.exit()
+
 setup(name="python-dicks",
-      version="1.0.0",
+      version=version,
       description='A Python client for the Dicks API',
       license="MIT",
       install_requires=["simplejson","requests"],
